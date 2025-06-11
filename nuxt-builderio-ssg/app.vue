d2e15ef276de156c7f87821dff02418e0a2d3613
Oliver Schafeld <oliver@schafeld.com>
2025-06-11 09:51:43 +0200

    add basic navigation

--- nuxt-builderio-ssg/app.vue
+++ nuxt-builderio-ssg/app.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
+    <Navigation />
     <!-- <NuxtRouteAnnouncer />
     <NuxtWelcome /> -->
     <NuxtPage />
   </div>
 </template>
+
+<script>
+import Navigation from './components/navigation.vue';
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+  },
+};
+</script>
---
+++ nuxt-builderio-ssg/components/navigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/about-me">About Me</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
+
+<style scoped>
+nav {
+  background-color: #f8f9fa;
+  padding: 1rem;
+}
+
+ul {
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  font-family: Arial, sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: #007bff;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
